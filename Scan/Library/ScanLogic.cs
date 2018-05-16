96b484c62252c3b2146f4197efe5c388982a9552
Svetlana <geksogen4ik@gmail.com>
2018-05-16 23:14:21 +0300

    recursion

---
+++ Scan/Library/ScanLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public static class ScanLogic
+    {
+           
+        public static List<string> Scan (string dirName)
+        {
+ 
+            List<string> allFolders = new List<string>(Directory.EnumerateDirectories(dirName));
+
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Catalogs:");
+
+                foreach (var s in allFolders)
+                {
+                   
+                    allFolders.AddRange(Scan(s));
+                }
+
+                Console.WriteLine("Files:");
+                string[] files = Directory.GetFiles(dirName);
+
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            return allFolders;
+        }
+
+    }
+}
--- Scan/Scan/Program.cs
+++ Scan/Scan/Program.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Library;
 
 namespace Scan
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            string dirName = " ";
+            ScanLogic.Scan(dirName);
+            Console.WriteLine("Enter the catalog's name, please");
+            try
+            {
+                dirName = Console.ReadLine();
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("I/O error");
+            }
+            catch(OutOfMemoryException)
+            {
+                Console.WriteLine("Memory is full");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Symbols amoun grater then MaxValue");
+            }
 
         }
     }
+   
 }
+    
+
+       
+    
+
+    
+        
+    
+
